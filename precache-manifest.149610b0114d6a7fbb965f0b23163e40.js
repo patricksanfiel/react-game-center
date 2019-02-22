dcef1d94b76c185ef25f16b3b1a133a0cf4dbfb3
Patrick Sanfiel <patricksanfiel@gmail.com>
2019-02-22 15:04:14 -0500

    Updates

---
+++ precache-manifest.149610b0114d6a7fbb965f0b23163e40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59e194279102dffac54e",
+    "url": "/react-hangman/static/js/runtime~main.59e19427.js"
+  },
+  {
+    "revision": "47370577cf66ce4d76fd",
+    "url": "/react-hangman/static/js/main.47370577.chunk.js"
+  },
+  {
+    "revision": "5cb7871d3228c9b26253",
+    "url": "/react-hangman/static/js/2.5cb7871d.chunk.js"
+  },
+  {
+    "revision": "47370577cf66ce4d76fd",
+    "url": "/react-hangman/static/css/main.4c3cafe3.chunk.css"
+  },
+  {
+    "revision": "f65df140b261973d03550ce0740cc0e1",
+    "url": "/react-hangman/index.html"
+  }
+];
